3532e633d16f3f26c2a7361dae6701ad9b0ec99b
Felipe Souto <134431973+FelipeSoutoo@users.noreply.github.com>
2023-11-28 14:17:32 -0300

    mostrar id do elemento na tabela

--- src/pages/register.php
+++ src/pages/register.php
@@ -51,6 +51,7 @@
             <h3>Insira as informações do jogo: </h3>
             <form id="gameForm">
                 <div class="geral">
+                <input type="hidden" name="id" id="id">
                     <div class="nome">
                         <label for="nome">Nome</label>
                         <input type="text" name="nome" id="nome">
@@ -60,7 +61,7 @@
                         <input type="text" name="genero" id="genero">
                     </div>
                     <div class="valor">
-                        <label for="valor">Valor</label>
+                        <label for="valor">Valor</label><br>
                         <input type="number" name="valor" id="valor">
                     </div>
                     <div class="plataforma">
@@ -97,7 +98,7 @@
                 // Send the form data to the PHP script using AJAX
                 $.ajax({
                     type: 'POST',
-                    url: 'crud/inserir.php',
+                    url: 'crud/insert.php',
                     data: formData,
                     success: function(response) {
                         // Handle the response from the PHP script
@@ -108,6 +109,7 @@
             });
 
             $('#enviarEdicao').on('click', function(){
+                var id = $('#id').val();
                 var nome = $('#nome').val();
                 var genero = $('#genero').val();
                 var valor = $('#valor').val();
@@ -115,7 +117,7 @@
 
                 // Create a JSON object with the form data
                 var formData = {
-                    'id': elementoEditado,
+                    'id': id,
                     'nome': nome,
                     'genero': genero,
                     'valor': valor,
@@ -170,11 +172,13 @@
             var row = $("#" + id); // Get the table row with the corresponding ID using jQuery
             var cells = row.find('td'); // Find all the table cells within the row
 
-            var current_nome = $(cells[0]).text(); // Retrieve the inner text of the first cell
-            var current_genero = $(cells[1]).text(); // Retrieve the inner text of the second cell
-            var current_valor = $(cells[2]).text(); // Retrieve the inner text of the third cell
-            var current_plataforma = $(cells[3]).text(); // Retrieve the inner text of the fourth cell
+            var current_id = $(cells[0]).text(); // Retrieve the inner text of the first cell
+            var current_nome = $(cells[1]).text(); // Retrieve the inner text of the first cell
+            var current_genero = $(cells[2]).text(); // Retrieve the inner text of the second cell
+            var current_valor = $(cells[3]).text(); // Retrieve the inner text of the third cell
+            var current_plataforma = $(cells[4]).text(); // Retrieve the inner text of the fourth cell
          
+            var id = $('#id').val(current_id);
             var nome = $('#nome').val(current_nome);
             var genero = $('#genero').val(current_genero);
             var valor = $('#valor').val(current_valor);
